6eb2c399ac2b5799aa886ac5b4acbeb65a79554b
Boris Dimitrov <b2@sysadmins-bg.net>
2014-10-08 19:24:25 +0300

    new file:   ex5.c

---
+++ week0/ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(int argc, char *argv[]) 
+{
+	char ch;
+	FILE *src, *dst;
+ 
+	src = fopen(argv[1], "r");
+	dst = fopen(argv[2], "w");
+
+  	while( ( ch = fgetc(src) ) != EOF ) {
+    	fputc(ch, dst);
+	}
+}
