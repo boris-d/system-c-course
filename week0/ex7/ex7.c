9aae8b2de9bbb15ab6bbc08c9196a622459ddd36
Boris Dimitrov <b2@sysadmins-bg.net>
2014-10-10 20:06:49 +0300

    new file:   ex1/Makefile new file:   ex1/ex1 new file:   ex2/Makefile new file:   ex3/Makefile new file:   ex4/Makefile new file:   ex5/MATE_DESKTOP_SESSION_ID=this-is-deprecated new file:   ex5/Makefile 	new file:   ex5/ex5 new file:   ex7/Makefile 	new file:   ex7/ex7 	new file:   ex7/ex7.c

---
+++ week0/ex7/ex7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+
+	char src, dst;
+
+	if ( rename(argv[1], argv[2]) == 0  )
+			{
+				printf("file is renamed\n");
+			}
+	return 0;
+}
